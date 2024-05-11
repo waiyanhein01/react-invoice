ad94b90646ad1063af483b313d2213ae6cdb06ce
Wai Yan Hein <wyan00196@gmail.com>
2024-05-11 23:43:51 +0630

    update-cost

--- src/components/CheckOutForm.jsx
+++ src/components/CheckOutForm.jsx
@@ -4,8 +4,8 @@ import { ProductContext } from "../context/ProductProvider";
 import { ItemContext } from "../context/ItemProvider";
 
 const CheckOutForm = () => {
-  const { products } = useContext(ProductContext);
-  const { addItem } = useContext(ItemContext);
+  const {products,} = useContext(ProductContext)
+  const {  addItem } = useContext(ItemContext)
   const selectRef = useRef();
   const quantityRef = useRef();
   const formRef = useRef();
@@ -15,23 +15,22 @@ const CheckOutForm = () => {
 
     const id = Date.now();
 
-    const currentProduct = products.find(
-      (product) => product.id === parseInt(selectRef.current.value)
-    );
+    const currentProduct = products.find(product => product.id === parseInt(selectRef.current.value))
 
     const quantity = parseInt(quantityRef.current.value);
 
-    const cost = quantity * parseFloat(currentProduct.price).toFixed(2);
+    const cost = (quantity * parseFloat(currentProduct.price)).toFixed(2);
 
     const newItem = {
       id,
-      product: currentProduct,
+      product : currentProduct,
       quantity,
-      cost,
-    };
+      cost
+    }
 
-    addItem(newItem);
+    addItem(newItem)
     formRef.current.reset();
+
 
     // console.log(currentProduct)
 
@@ -60,13 +59,7 @@ const CheckOutForm = () => {
             <div className="mb-2 block">
               <Label htmlFor="base" value="Quantity" />
             </div>
-            <TextInput
-              ref={quantityRef}
-              id="base"
-              type="number"
-              sizing="md"
-              required
-            />
+            <TextInput ref={quantityRef} id="base" type="number" sizing="md" required/>
           </div>
 
           <div className=" col-span-1 ">
