1edd3fb61719ef28bba3f5b7ec54dd7b32e826b7
Wai Yan Hein <wyan00196@gmail.com>
2024-06-06 22:18:41 +0630

    update

--- src/components/CheckOutForm.jsx
+++ src/components/CheckOutForm.jsx
@@ -4,8 +4,8 @@ import { ProductContext } from "../context/ProductProvider";
 import { ItemContext } from "../context/ItemProvider";
 
 const CheckOutForm = () => {
-  const {products,} = useContext(ProductContext)
-  const {  addItem } = useContext(ItemContext)
+  const { products } = useContext(ProductContext);
+  const { addItem } = useContext(ItemContext);
   const selectRef = useRef();
   const quantityRef = useRef();
   const formRef = useRef();
@@ -15,7 +15,9 @@ const CheckOutForm = () => {
 
     const id = Date.now();
 
-    const currentProduct = products.find(product => product.id === parseInt(selectRef.current.value))
+    const currentProduct = products.find(
+      (product) => product.id === parseInt(selectRef.current.value)
+    );
 
     const quantity = parseInt(quantityRef.current.value);
 
@@ -23,14 +25,13 @@ const CheckOutForm = () => {
 
     const newItem = {
       id,
-      product : currentProduct,
+      product: currentProduct,
       quantity,
-      cost
-    }
+      cost,
+    };
 
-    addItem(newItem)
+    addItem(newItem);
     formRef.current.reset();
-
 
     // console.log(currentProduct)
 
@@ -44,7 +45,7 @@ const CheckOutForm = () => {
         <div className=" grid grid-cols-5 gap-3">
           <div className=" col-span-2">
             <div className="mb-2 block">
-              <Label htmlFor="countries" value="Select Fruit" />
+              <Label htmlFor="countries" value="Select Fruits" />
             </div>
             <Select ref={selectRef} id="countries" required>
               {products.map(({ id, title }) => (
@@ -59,7 +60,13 @@ const CheckOutForm = () => {
             <div className="mb-2 block">
               <Label htmlFor="base" value="Quantity" />
             </div>
-            <TextInput ref={quantityRef} id="base" type="number" sizing="md" required/>
+            <TextInput
+              ref={quantityRef}
+              id="base"
+              type="number"
+              sizing="md"
+              required
+            />
           </div>
 
           <div className=" col-span-1 ">
